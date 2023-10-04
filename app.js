404beb98b78c9313b6abce6e3816d5c39702f58b
IliyasO7 <shaikhiliyas343@gmail.com>
2023-10-05 01:39:22 +0530

    assosiatio 1 to many and routes

--- app.js
+++ app.js
@@ -2,18 +2,28 @@ import express from 'express';
 import dotenv from 'dotenv';
 import sequelize from './utils/database.js';
 import { sendResponse } from './utils/helper.js';
+import routes from './routes/index.js';
+import User from './models/user.js';
+import Todo from './models/todo.js';
+
 dotenv.config();
-
-
 
 const app = express();
 
 
+app.use(routes);
+
+app.get('/', (req, res) => {
+  sendResponse(res, 200, 'Server is working');
+});
 
 app.use((err, req, res, next) => {
     console.error(err);
-    sendResponse(res, 500, 'Internal server error', null, err);
+    sendResponse(res, 500, 'internal server error', null, err);
   });
+
+User.hasMany(Todo);  
+Todo.belongsTo(User);
 
 sequelize.sync()
   .then(() => {
