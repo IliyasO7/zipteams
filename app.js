528958c699d1d60cacaf0a1d4a5ff2cef02e70f0
IliyasO7 <shaikhiliyas343@gmail.com>
2023-10-05 18:01:54 +0530

    refactored

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 
   res.header('Access-Control-Allow-Origin', '*'); 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); 
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization,x-api-key'); 
 
 
   if (req.method === 'OPTIONS') {
