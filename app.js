7521e71a35a83f4cf621a69b3e2dfb5ff7cf6989
IliyasO7 <shaikhiliyas343@gmail.com>
2023-10-05 18:01:06 +0530

    set-headers

--- app.js
+++ app.js
@@ -11,9 +11,21 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.options('/*', (_, res) => {
-  res.sendStatus(200);
+
+app.use((req, res, next) => {
+
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); 
+
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200); 
+  } else {
+    next();
+  }
 });
+
 
 app.use(express.json())
 
