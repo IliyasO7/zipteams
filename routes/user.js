97e4ed6022404f1716e56fc90c3f4ed1a643130c
IliyasO7 <shaikhiliyas343@gmail.com>
2023-10-05 17:09:35 +0530

    added Verify-API Key for deleting users secuity,

--- routes/user.js
+++ routes/user.js
@@ -1,21 +1,25 @@
 import { Router } from 'express';
 import * as controller from '../controllers/user.js';
 import * as validation from '../validation/user.js';
-import { validate } from '../utils/helper.js';
-
+import { validate,verifyApiKey } from '../utils/helper.js';
+import { checkAuth } from '../middlewares/checkAuth.js';
 
 const router = Router();
 
 router
   .route('/')
   .get(controller.getUsers)
-  .post(validate(validation.createUser), controller.createUser);
+  .post(verifyApiKey,validate(validation.createUser), controller.createUser);
+
+router
+  .route('/login')
+  .post(validate(validation.login), controller.login);
 
 router
   .route('/:id')
   .get(controller.getUserById)
-  .patch(validate(validation.updateUser), controller.updateUser)
-  .delete(controller.deleteUser);
+  .patch(validate(validation.updateUser),checkAuth(), controller.updateUser)
+  .delete(verifyApiKey,controller.deleteUser);
 
 
 export default router;
